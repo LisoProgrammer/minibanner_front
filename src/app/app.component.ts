fe13303b4b8013a6cd5aaf63baa0a27db771ba6f
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-02 11:05:13 -0500

    Se incluye en el componente ts principal el cubcompoente de tabla de alumnos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormConsultarStdComponent } from "./form-consultar-std/form-consultar-std.component";
-
+import { TableStdComponent } from './table-std/table-std.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormConsultarStdComponent],
+  imports: [RouterOutlet, FormConsultarStdComponent, TableStdComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
