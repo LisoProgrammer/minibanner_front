a60ed94543b59d25ba188b5a4d11f0bd6156b57d
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-06 21:10:25 -0500

    Para los formularios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/
 import { RouterOutlet } from '@angular/router';
 import { FormConsultarStdComponent } from './form-consultar-std/form-consultar-std.component';
 import { TableStdComponent } from './table-std/table-std.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,  // Define el componente como standalone
-  imports: [RouterOutlet, FormConsultarStdComponent, TableStdComponent],  // Agrega tus otros componentes aquí
+  imports: [RouterOutlet, FormConsultarStdComponent, TableStdComponent, ReactiveFormsModule],  // Agrega tus otros componentes aquí
   providers: [
   ],
   templateUrl: './app.component.html',
