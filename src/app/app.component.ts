89efcfcd764798db174e415c600df8ab617608ed
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-07 16:14:36 -0500

    Incluido el servicio de alumnos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { FormConsultarStdComponent } from './form-consultar-std/form-consultar-std.component';
 import { TableStdComponent } from './table-std/table-std.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AlumnosService } from './servicios/alumnos/alumnos.service';
 @Component({
   selector: 'app-root',
   standalone: true,  // Define el componente como standalone
   imports: [RouterOutlet, FormConsultarStdComponent, TableStdComponent, ReactiveFormsModule],  // Agrega tus otros componentes aquí
   providers: [
+    AlumnosService
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
