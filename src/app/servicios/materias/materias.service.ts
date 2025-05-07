7029741422e22a3308830e8f61bdd5917aa7edde
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-07 16:44:12 -0500

    Se adiciono el metodo que extrae las materias de la API

--- src/app/servicios/materias/materias.service.ts
+++ src/app/servicios/materias/materias.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class MateriasService {
-
-  constructor() { }
+  private url = 'http://127.0.0.1:80/minibanner_backend/backend_minibanner/apis/materias/';
+  constructor(private http: HttpClient) { }
+  get_materias(){
+    return this.http.get(this.url+"list_materias.php");
+  }
 }
