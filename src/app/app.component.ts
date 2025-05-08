ee772d5c4012ae8eb9843b087e3f2193679db780
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-08 14:13:57 -0500

    Restructuracion del proyecto por rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';  // Importa HttpClient y provideHttpClient
-import { RouterOutlet } from '@angular/router';
-import { Auth } from './servicios/auth/auth.service';
-import { FormConsultarStdComponent } from './form-consultar-std/form-consultar-std.component';
-import { TableStdComponent } from './table-std/table-std.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AlumnosService } from './servicios/alumnos/alumnos.service';
+import { Router, RouterOutlet } from '@angular/router';
+
+// app.component.ts
 @Component({
+  standalone: true,
   selector: 'app-root',
-  standalone: true,  // Define el componente como standalone
-  imports: [RouterOutlet, FormConsultarStdComponent, TableStdComponent, ReactiveFormsModule],  // Agrega tus otros componentes aquí
-  providers: [
-    AlumnosService, Auth
-  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet]
 })
-export class AppComponent {
-  title = 'minibanner';
-  
-}
+export class AppComponent {}
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfesorComponent } from './profesor/profesor.component';
+import { StudentComponent } from './student/student.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'profesor', component: ProfesorComponent },
+  { path: 'student', component: StudentComponent },
+];
