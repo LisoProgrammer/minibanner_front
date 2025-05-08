2de62b67cb3133eea51faee6dff62ec47d9b026e
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-08 14:15:34 -0500

    Se copia el contenido de lo que estaba antes en la app principal

--- src/app/profesor/profesor.component.ts
+++ src/app/profesor/profesor.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';  // Importa HttpClient y provideHttpClient
-import { RouterOutlet } from '@angular/router';
-import { Auth } from '../servicios/auth/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../servicios/auth/auth.service';
 import { FormConsultarStdComponent } from '../form-consultar-std/form-consultar-std.component';
 import { TableStdComponent } from '../table-std/table-std.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AlumnosService } from '../servicios/alumnos/alumnos.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-profesor',
-  standalone: true,  // Define el componente como standalone
-  imports: [RouterOutlet, FormConsultarStdComponent, TableStdComponent, ReactiveFormsModule],  // Agrega tus otros componentes aquí
-  providers: [
-    AlumnosService, Auth
-  ],
+  standalone: true,
+  imports: [RouterOutlet, FormConsultarStdComponent, TableStdComponent, ReactiveFormsModule],
+  providers: [AlumnosService, AuthService],
   templateUrl: './profesor.component.html',
   styleUrl: './profesor.component.css'
 })
-export class ProfesorComponent {
+export class ProfesorComponent implements OnInit {
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.check_sesion().subscribe({
+      next: (res) => {
+        if (!res.loggIn || res.user.rol !== 1) {
+          alert('Acceso denegado. Debe iniciar sesión como profesor.'+res.authenticated+" "+res.user.rol);
+          this.router.navigate(['/login']);
+        }
+      },
+      error: (err) => {
+        console.error('Error verificando sesión:', err);
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
