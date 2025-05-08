5720929d799a4a6ef53b449402b8e0edc2a2e0af
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-08 14:14:52 -0500

    Se definen los metodos del servicio de autenticacion

--- src/app/servicios/auth/auth.service.ts
+++ src/app/servicios/auth/auth.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
-export class Auth {
+export class AuthService {
   private url = 'http://127.0.0.1:80/minibanner_backend/backend_minibanner/auth/';
-  constructor(private http: HttpClient) { }
-  auth(data: any){
-    this.http.post(this.url+'login.php', data)
-      .subscribe({
-        next: response => {
-          console.log(response);
-        },
-        error: error => {
-          console.error('Error al enviar:', error);
-          alert('Error al enviar los datos.');
-        }
-      });
+
+  constructor(private http: HttpClient) {}
+
+  login(id: number, password: string): Observable<any> {
+    const body = JSON.stringify({ id_user: id, password: password });
+  
+    return this.http.post(this.url + 'login.php', body, {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+  check_sesion(): Observable<any> {
+    return this.http.get(this.url+'check_sesion.php', {
+      withCredentials: true // Importante para enviar cookies de sesión
+    });
   }
 }
