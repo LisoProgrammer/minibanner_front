3cca463488389462295449763efedac384dbe19d
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-09 18:30:11 -0500

    Se insertan las notas del estudiante

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { NavbarComponent } from "../navbar/navbar.component";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { AlumnoPService } from '../servicios/alumno_p/alumno-p.service';
+import { Materia } from '../models/materia';
 
 @Component({
   selector: 'app-student',
-  imports: [NavbarComponent],
+  standalone: true,
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './student.component.html',
-  styleUrl: './student.component.css'
+  styleUrls: ['./student.component.css']
 })
-export class StudentComponent {
+export class StudentComponent implements OnInit {
+  materias: Materia[] = [];
 
+  constructor(private alumnopService: AlumnoPService) {}
+
+  ngOnInit(): void {
+    this.cargarMaterias();
+  }
+
+  cargarMaterias(): void {
+    this.alumnopService.getMyNotes().subscribe((data: Materia[]) => {
+      this.materias = data;
+      console.log(this.materias)
+    });
+  }
 }
