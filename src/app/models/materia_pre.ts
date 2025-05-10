f27088e9c7488e468a9864360e9ecbc67411c629
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-10 10:30:28 -0500

    Para saber si es una Materia con prediccion del tercer corte

---
+++ src/app/models/materia_pre.ts
@@ -0,0 +1,11 @@
+export interface MateriaPre{
+    code: string;
+    creditos: number;
+    nombre: string;
+    nota_1: number;
+    nota_2: number;
+    nota_3: number;
+    promedio: number;
+    obs: string;
+    prediccion: boolean;
+}
