617a9a3f83314580b9c2140a4452bbbd9b0784ff
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-10 10:31:23 -0500

    Componente de estadisticas del profesor

---
+++ src/app/profesor/staticsp/staticsp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staticsp',
+  imports: [],
+  templateUrl: './staticsp.component.html',
+  styleUrl: './staticsp.component.css'
+})
+export class StaticspComponent {
+
+}
