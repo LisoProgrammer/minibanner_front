ea7b3b74843c33731699ce32bdd34db100435898
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-10 17:26:48 -0500

    Se evalua la sesion

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,36 +9,59 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent implements OnInit {
   user: any = null;
+  sesionVerificada: boolean = false;
+  route: string = '';
   constructor(private authService: AuthService, private router: Router) {}
+
   ngOnInit(): void {
-      this.get_sesion();
+    this.route = this.router.url;
+    console.log(this.route)
+    this.get_sesion();
   }
-  get_sesion(): void{
+
+  get_sesion(): void {
     this.authService.check_sesion().subscribe({
-      next: (data: any) =>{
+      next: (data: any) => {
         console.log(data);
-        if(data.log_in){
+        if (data.log_in) {
           this.user = data.user as User;
+        }else{
+          this.router.navigate(['login']);
         }
-      }
-    })
+        this.sesionVerificada = true; // ← solo ahora se considera completa la verificación
+      },
+      error: () => {
+        //this.sesionVerificada = true;
+      },
+    });
   }
-  go_data(): void{
-    if(this.user.rol == 0){
-      this.router.navigate(['/student/data']);
-    }else if(this.user.rol == 1){
-      this.router.navigate(['/profesor/data']);
+  go_home(): void{
+    if (this.user.rol == 0) {
+      this.router.navigate(['student']);
+      //alert("Se redirecciona a student")
+    } else if (this.user.rol == 1) {
+      this.router.navigate(['profesor']);
+      //alert("Se redirecciona a profesor")
     }
   }
-  go_statics(): void{
-    if(this.user.rol == 0){
-      this.router.navigate(['/student/statics']);
-    }else if(this.user.rol == 1){
-      this.router.navigate(['/profesor/statics']);
+  go_data(): void {
+    if (this.user.rol == 0) {
+      this.router.navigate(['student/data']);
+      //alert("Se redirecciona a student/data")
+    } else if (this.user.rol == 1) {
+      this.router.navigate(['profesor/data']);
+      //alert("Se redirecciona a profesor/data")
+    }
+  }
+  go_statics(): void {
+    if (this.user.rol == 0) {
+      this.router.navigate(['student/statics']);
+    } else if (this.user.rol == 1) {
+      this.router.navigate(['profesor/statics']);
     }
   }
   cerrarSesion(): void {
@@ -48,8 +71,8 @@ export class NavbarComponent implements OnInit{
       },
       error: (err) => {
         console.error('Error al cerrar sesión', err);
-        //this.router.navigate(['/login']); // forzar regreso al login igual
-      }
+        this.router.navigate(['/login']); // forzar regreso al login igual
+      },
     });
   }
 }
