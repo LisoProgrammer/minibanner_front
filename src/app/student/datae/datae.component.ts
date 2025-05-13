1d07c273e419565f7939d602794e8d6d751774ff
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-12 19:27:12 -0500

    Se consultan e insertan los datos del alumno

--- src/app/student/datae/datae.component.ts
+++ src/app/student/datae/datae.component.ts
@@ -4,18 +4,24 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from '../../servicios/auth/auth.service';
 import { Router } from '@angular/router';
 import { StudentComponent } from "../student.component";
-
+import { AlumnoPService } from '../../servicios/alumno_p/alumno-p.service';
+import { Alumno } from '../../models/alumno';
+import { FormChangePassComponent } from '../../form-change-pass/form-change-pass.component';
 @Component({
   selector: 'app-datae',
   standalone: true,
-  imports: [NavbarComponent, CommonModule],
+  imports: [NavbarComponent, CommonModule, FormChangePassComponent],
   templateUrl: './datae.component.html',
   styleUrl: './datae.component.css'
 })
 export class DataeComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  alumno: Alumno | null = null;
+  constructor(private AlumnopService: AlumnoPService) {}
 
   ngOnInit(): void {
-    //
+    this.AlumnopService.getMyInfo().subscribe((data: Alumno) =>{
+      this.alumno = data as Alumno;
+      console.log(this.alumno)
+    })
   }
 }
