6f871a84ab586c58e1978ef6478823c24bf21236
Lisandro Zapata <zlisandro5@gmail.com>
2025-05-14 06:51:41 -0500

    Se insertaron las estadisticas

--- src/app/servicios/alumnos/alumnos.service.ts
+++ src/app/servicios/alumnos/alumnos.service.ts
@@ -4,56 +4,57 @@ import { BehaviorSubject } from 'rxjs';
 import { Alumno } from '../../models/alumno';
 declare let Swal: any;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AlumnosService {
-
-  private url = 'http://localhost:80/minibanner_backend/backend_minibanner/apis/alumnos/'; // Replace with your API URL
+  private url =
+    'http://localhost:80/minibanner_backend/backend_minibanner/apis/alumnos/'; // Replace with your API URL
   private alumnoSeleccionadoSource = new BehaviorSubject<Alumno | null>(null);
   alumnoSeleccionado$ = this.alumnoSeleccionadoSource.asObservable();
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   seleccionarAlumno(alumno: Alumno) {
     this.alumnoSeleccionadoSource.next(alumno);
   }
   getAlumnos() {
     //console.log(this.url+"get_alumnos.php");
-    return this.http.get(this.url+"get_alumnos.php");
+    return this.http.get(this.url + 'get_alumnos.php');
   }
-  deleteAlumno(id_alumno: number){
-    console.log("Estudiante eliminado");
-    return this.http.get(this.url+"delete_alumno.php?id_alumno="+id_alumno);
+  deleteAlumno(id_alumno: number) {
+    console.log('Estudiante eliminado');
+    return this.http.get(this.url + 'delete_alumno.php?id_alumno=' + id_alumno);
   }
-  insertAlumno(data: any){
-    this.http.post(this.url+'insert_alumno.php', data)
-      .subscribe({
-        next: response => {
-          console.log('Respuesta del servidor:', response);
-          window.location.reload();
-          alert('Alumno creado correctamente.');
-        },
-        error: error => {
-          console.error('Error al enviar:', error);
-          alert('Error al enviar los datos.');
-        }
-      });
+  insertAlumno(data: any) {
+    this.http.post(this.url + 'insert_alumno.php', data).subscribe({
+      next: (response) => {
+        console.log('Respuesta del servidor:', response);
+        window.location.reload();
+        alert('Alumno creado correctamente.');
+      },
+      error: (error) => {
+        console.error('Error al enviar:', error);
+        alert('Error al enviar los datos.');
+      },
+    });
   }
-  updateAlumno(data: any){
-    this.http.post(this.url+'update_alumno.php', data)
-      .subscribe({
-        next: response => {
-          console.log('Respuesta del servidor:', response);
-          window.location.reload();
-          alert('Alumno actualizado correctamente.');
-        },
-        error: error => {
-          console.error('Error al enviar:', error);
-          alert('Error al enviar los datos.');
-        }
-      });
+  updateAlumno(data: any) {
+    this.http.post(this.url + 'update_alumno.php', data).subscribe({
+      next: (response) => {
+        console.log('Respuesta del servidor:', response);
+        window.location.reload();
+        alert('Alumno actualizado correctamente.');
+      },
+      error: (error) => {
+        console.error('Error al enviar:', error);
+        alert('Error al enviar los datos.');
+      },
+    });
   }
   guardarNotas(data: any) {
-  return this.http.post(this.url+'notas/change_nota.php', data, {
-    withCredentials: true
-  });
-}
+    return this.http.post(this.url + 'notas/change_nota.php', data, {
+      withCredentials: true,
+    });
+  }
+  get_metrics() {
+    return this.http.get(this.url + 'statics/get_metrics.php');
+  }
 }
